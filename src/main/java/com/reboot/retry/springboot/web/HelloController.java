c84505d1d27bc8d36573b4c7d41731f7253457a6
nicetfour <tfour@hanmail.net>
2020-09-18 07:51:59 +0900

    main controller반영

---
+++ src/main/java/com/reboot/retry/springboot/web/HelloController.java
@@ -0,0 +1,10 @@
+package com.reboot.retry.springboot.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping()
+    public String
+}
