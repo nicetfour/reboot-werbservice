1148940d3d3ef17698f76371b0cfc9e0360ac003
nicetfour <tfour@hanmail.net>
2020-09-22 07:24:17 +0900

    20200922 반영

--- src/main/java/com/reboot/retry/springboot/web/HelloController.java
+++ src/main/java/com/reboot/retry/springboot/web/HelloController.java
@@ -1,12 +1,23 @@
 package com.reboot.retry.springboot.web;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.reboot.retry.springboot.web.dto.HelloResponseDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @RequestMapping("/hello")
+
+    @GetMapping("/hello")
     public String hello() {
         return "hello world!!!";
     }
+
+    @GetMapping("/hello/dto")
+    public String hello(@RequestParam("name") String name,
+                        @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
+    }
 }
+
+
